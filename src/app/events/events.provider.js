1901b0eafadb3cbc98adcc9a7f10cc608ebef2f6
code_master5 <lauhny.bimalk@gmail.com>
2017-08-16 01:52:36 +0530

    Separate events folder

--- src/app/events/events.provider.js
+++ src/app/events/events.provider.js
@@ -1,8 +1,17 @@
+/**
+ * @file      events.provider.js
+ * @author    Bimalkant Lauhny <lauhny.bimalk@gmail.com>
+ * @copyright MIT License
+ * @brief     Creates an event emitter using a subject observable
+ *            to which different services can subscribe
+ */
 (function () {
   'use strict';
 
   angular.module("janusHangouts.eventsObservable", [])
     .provider('jhEventsObservable', function () {
+      /* Rx object */
+      var Rx = window.Rx;
       var eventsObservable = new Rx.Subject();
 
       return {
